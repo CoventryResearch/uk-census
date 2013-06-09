9c1b74f2616ee03e5d63d0b03df5fefa8fcb89b6
Samuel Leach <samuel.leach@gmail.com>
2013-06-09 13:06:56 +0100

    Adding code generator for creating rails model and seeded code

---
+++ csv_model_seed_generator.rb
@@ -0,0 +1,32 @@
+require 'csv'
+
+csvfile = 'census_by_postcodedistrict.csv'
+model = 'CensusCount'
+data = CSV.read(csvfile, { :col_sep => ","})
+
+
+generator_command = "rails generate active_record:model #{model} "
+data[0].each_with_index do |label, i|
+	if i == 0
+		generator_command = generator_command + label+':string '
+	else
+		generator_command = generator_command + label+':integer '
+	end
+end
+puts '###### Generator command ########'
+puts generator_command
+puts ''
+
+seed_command = "#{model}.create({"
+data[0].each_with_index do |label, i|
+	seed_command = seed_command + label + ": row[#{i}], "
+end
+seed_command = seed_command[0...-2] # Remove last ', '
+seed_command = seed_command + "})"
+
+
+puts '###### Seeder commands ########'
+puts "parsed_file = CSV.read(\"#{csvfile}\", { :col_sep => \",\", :headers=>:first_row})"
+puts 'parsed_file.each do |row|'
+puts '   '+seed_command
+puts 'end'
