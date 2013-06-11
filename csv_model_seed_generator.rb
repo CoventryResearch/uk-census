73b28637b03edb5cb1adc6becaeb1ad6b4980f7d
Samuel Leach <samuel.leach@gmail.com>
2013-06-11 10:27:38 +0100

    Setting variable names to lower case

--- csv_model_seed_generator.rb
+++ csv_model_seed_generator.rb
@@ -7,6 +7,7 @@ data = CSV.read(csvfile, { :col_sep => ","})
 
 generator_command = "rails generate active_record:model #{model} "
 data[0].each_with_index do |label, i|
+	label = label.downcase
 	if i == 0
 		generator_command = generator_command + label+':string '
 	else
@@ -19,14 +20,14 @@ puts ''
 
 seed_command = "#{model}.create({"
 data[0].each_with_index do |label, i|
-	seed_command = seed_command + label + ": row[#{i}], "
+	seed_command = seed_command + label.downcase + ": row[#{i}], "
 end
 seed_command = seed_command[0...-2] # Remove last ', '
 seed_command = seed_command + "})"
 
 
 puts '###### Seeder commands ########'
-puts "parsed_file = CSV.read(\"#{csvfile}\", { :col_sep => \",\", :headers=>:first_row})"
+puts "parsed_file = CSV.read(\"public/data/#{csvfile}\", { :col_sep => \",\", :headers=>:first_row})"
 puts 'parsed_file.each do |row|'
 puts '   '+seed_command
 puts 'end'
