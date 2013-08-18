6d070dfd5ce5c9f4aa4a6732a60920afeb53b65a
Samuel Leach <samuel.leach@gmail.com>
2013-08-18 15:50:09 +0100

    Initial commit

--- Makefile
+++ Makefile
@@ -1,17 +1,27 @@
 GEOLYTIX=http://geolytix.co.uk/images/
 
-data/Census11Data.zip:
+AREACODES = MSOACode LSOACode LADCode WardCode SecLGCode FmCntyCode PCOCode SHACode ConstCode RegionCode
+
+all: $(addprefix data/Census11_by_, $(addsuffix .csv, $(AREACODES)))
+
+# Look up table is missing MSOA codes for some Northern Ireland OAs
+#
+# [N00000001 N00000002 N00000003 ..., N00004535 N00004536 N00004537] /No MSOA codes
+
+# What is ConstCode RegionCode
+
+clean: rm -rf data log
+
+data/%.zip:
 	mkdir -p $(dir $@) && wget $(GEOLYTIX)/$(notdir $@) -O $@.download && mv $@.download $@
 	touch $@
 
-data/OA11LookUp.zip:
-	mkdir -p $(dir $@) && wget $(GEOLYTIX)/$(notdir $@) -O $@.download && mv $@.download $@
-	touch $@
-
-data/Census11Data.txt: data/Census11Data.zip
+data/%.txt: data/%.zip
 	unzip -u $< -d $(dir $@)
 	touch $@
 
-data/OA11LookUp.txt: data/OA11LookUp.zip
-	unzip -u $< -d $(dir $@)
-	touch $@
+# Currently 583 seconds, 35643 LSOAs; 124 seconds, 7202 MSOAs; 20 seconds, 374 LADCode
+data/Census11_by_%.csv: data/OA11LookUp.txt data/Census11Data.txt census_to_lsoa.py
+	mkdir -p $(dir $@)
+	mkdir -p log
+	time python census_to_lsoa.py $*
---
+++ census_to_lsoa.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import time
+import logging
+import sys
+
+category = sys.argv[1]
+
+logfile = 'log/census_to_' + category + '.log'
+print "Opening log file " + logfile
+logging.basicConfig(filename=logfile, level=logging.DEBUG)
+
+lookupfile = "data/OA11LookUp.txt"
+print 'Reading lookup file' + lookupfile
+lookupdata = pd.read_csv(lookupfile, delimiter='\t')
+# lookupdata = lookupdata[0:999]
+
+censusfile = 'data/Census11Data.txt'
+print 'Reading census file '+censusfile
+censusdata = pd.read_csv(censusfile, delimiter='\t', na_values=[-1])
+for col in ['oaid', 'popx', 'popy']:
+	censusdata = censusdata.drop(col, axis=1)
+
+print "Grouping lookup data by " + category
+grouped_lookup = lookupdata.groupby(category)
+size = grouped_lookup.size()
+
+print "Summing census data over " + category + " values"
+rows_list = []
+group_list = []
+for name, group in grouped_lookup:
+	oacode_list = group['OACode'].values
+	# print name
+	# print oacode_list
+	filtered_censusdata = censusdata[censusdata['oacode'].isin(oacode_list)] # Bottleneck
+	summed_filtered_censusdata = filtered_censusdata.sum()
+	rows_list.append(summed_filtered_censusdata)
+	group_list.append(name)
+
+print 'Creating data frame'
+df = pd.DataFrame(rows_list)
+
+print 'Appending ' + category + ' ids'
+df[category] = group_list
+
+print 'Reordering ' + category + ' column'
+df = df.reindex(columns=pd.Index([category]).append(df.columns - [category]))
+
+outfile = 'data/Census11_by_'+category+'.csv'
+print 'Writing to ' + outfile
+df.to_csv(outfile, sep=',', index_label='index')
+
+print 'Done'
